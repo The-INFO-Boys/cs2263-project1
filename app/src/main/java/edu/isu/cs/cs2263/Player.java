3cadc668dbc37daa8af82be8f169e474ef525a46
David Lape <lapedavi@isu.edu>
2021-11-06 17:05:55 -0600

    Fixed Player Class

--- app/src/main/java/edu/isu/cs/cs2263/Player.java
+++ app/src/main/java/edu/isu/cs/cs2263/Player.java
@@ -1,38 +1,11 @@
 package edu.isu.cs.cs2263;
 
 import java.util.List;
-import java.util.ArrayList;
 
 public class Player<string, Stocks, list> {
-    public static list<Tiles> Tiles;
+    private List<Tile> Tiles;
 
-    public string Name;
+    private string Name;
 
-    public int Money;
-
-    public list Stocks;
-
-    public static void playTile(Tile) {
-
-    }
-
-    public static <Tiles> void foundHotel(list<Tiles>) {
-
-    }
-
-    public static void mergeHotels(list<Hotel>){
-
-    }
-
-    public static void sellStock(Hotel, list<Stock>){
-
-    }
-
-    public static void tradeStock(Hotel, list<Stock>){
-
-    }
-
-    public static void buyStock(Hotel){
-
-    }
+    private int Money;
 }
