f01c5d09fd4e10e283fae9ebf40500a104f29b4d
fernando-larios-71 <larifern@isu.edu>
2021-11-27 14:16:12 -0700

    javadoc updated for interfaces

--- app/src/main/java/edu/isu/cs/cs2263/StockIterator.java
+++ app/src/main/java/edu/isu/cs/cs2263/StockIterator.java
@@ -3,7 +3,10 @@ package edu.isu.cs.cs2263;
 public interface StockIterator {
 
     /**
-     *
+     *This is a java interface for the stock iterator.
+     *The nextStock method gets the next stock in the array.
+     * The hasMoreStock is a boolean, and it checks the array to see if
+     * there is more stock.
      * @return
      */
 
